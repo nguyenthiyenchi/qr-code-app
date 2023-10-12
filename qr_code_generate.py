667637941595d47284006573c5a5943148f6d0e6
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2023-10-12 23:20:22 +0700

    add function to the generate-btn

--- qr_code_generate.py
+++ qr_code_generate.py
@@ -5,11 +5,46 @@ import qrcode
 from PIL import Image, ImageTk
 from io import BytesIO
 
+# function to change size
+def change_window_size(width, height):
+    window.geometry(f"{width}x{height}")
 
+# function to set window in center of screen
+def center_window(window):   
+    window.update_idletasks()
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+    window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 # function to generate QR codes 
 def generate_qr_code():
     text = entry.get()  # retrieve and the text entered by the user in a variable
+    
+    change_window_size(360, 450)
+    center_window(window)
+
+    # middle panel contains QR Code
+    middle_panel = tk.Frame(window, bg=bgColor, width=360, height=200)
+    middle_panel.pack(fill=tk.BOTH, expand=True)
+    
+    qr_code_label = tk.Label(middle_panel, bg=bgColor)
+    qr_code_label.pack(pady=10)
+
+    # # bottom panel contains QR Code
+    bottom_panel = tk.Frame(window, bg=bgColor)
+    bottom_panel.pack(fill=tk.BOTH, expand=True)
+
+
+    # qr_code_label = tk.Label(bottom_panel, bg=bgColor)
+    # qr_code_label.pack(pady=10)
+
+    # # button to download the QR code image
+    # download_btn = tk.Button(bottom_panel, text="Download QR Code", command=save_qr_code, fg="#d4c0f1", bg="#260033", borderwidth=5, relief="flat")
+    # download_btn.pack(pady=10)
 
     qr = qrcode.QRCode(
         version = 1,
@@ -25,8 +60,8 @@ def generate_qr_code():
     # set color and size (width, height) for qr code image
     img = qr.make_image(fill_color="#d4c0f1cc", back_color="#260033cc")
     img = img.resize((200, 200), Image.ANTIALIAS)
-
     photo = ImageTk.PhotoImage(img)
+
     qr_code_label.config(image = photo)
     qr_code_label.image = photo
 
@@ -43,7 +78,7 @@ def save_qr_code():
 
 window = tk.Tk()
 window.title("QR Code Generator")
-window.geometry("360x450")
+window.geometry("360x200")
 window.configure(bg='#330044')
 window.resizable(False, False)
 
@@ -76,36 +111,17 @@ def restore_placeholder(event):
         entry.config(fg="#4b0082")
 
 # input
-entry = tk.Entry(top_panel, textvariable=entry_text, width=40, fg="#8a51ab")
+entry = tk.Entry(top_panel, textvariable=entry_text, borderwidth=5, relief="flat", width=40, fg="#8a51ab")
 entry.insert(0, "Enter a link")
 entry.bind("<FocusIn>", clear_placeholder)
 entry.bind("<FocusOut>", restore_placeholder)
 entry.pack(pady=(10, 20))
 
 # generate-btn
-generate_btn = tk.Button(top_panel, text="Generate QR Code", command=generate_qr_code, bg='#9665dd', fg=bgColor, borderwidth=0, relief="flat", activebackground='#9665dd')
-generate_btn.pack(pady=10)
+generate_btn = tk.Button(top_panel, text="Generate QR Code", command=generate_qr_code, bg='#9665dd', fg=bgColor, borderwidth=5, relief="flat", activebackground=bgColor)
+generate_btn.pack(pady=(10, 0))
 
-# bottom panel contains QR Code
-bottom_panel = tk.Frame(window, bg=bgColor)
-bottom_panel.pack(fill=tk.BOTH, expand=True)
-
-qr_code_label = tk.Label(bottom_panel, bg=bgColor)
-qr_code_label.pack(pady=10)
-
-# button to download the QR code image
-download_btn = tk.Button(bottom_panel, text="Download QR Code", command=save_qr_code, fg="#d4c0f1", bg="#260033", borderwidth=0, relief="flat")
-download_btn.pack(pady=10)
-
-# Center the main window on the screen
-window.update_idletasks()
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-x = (screen_width - window_width) // 2
-y = (screen_height - window_height) // 2
-window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+center_window(window)
 
 # start the app
 window.mainloop()
